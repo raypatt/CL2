c6e00cc55b2c2ff642e8cbd134fdbc0dd21e5fed
theBarg <44345306+theBarg@users.noreply.github.com>
2020-04-28 18:19:23 -0400

    Add files via upload

--- cnn+preprocess+eval[Word2Vec].py
+++ cnn+preprocess+eval[Word2Vec].py
@@ -27,7 +27,18 @@ conda install -c powerai sentencepiece
 from gensim.models import Word2Vec
 import sys
 import re
+import numpy
 
+def get_humor():
+  d={}
+  with open("humor_dataset.csv", 'r') as humor:
+    for line in humor:
+      elements=line.split(',')
+      word=elements[0]
+      score=elements[1]
+      d[word]=score
+  return d
+    
 
 def get_tokens(filePath): 
     listOfTokens1 = []
@@ -67,12 +78,18 @@ def convert_tokens_to_max(listOfTokens,max_length):
                 sentence.append(pad_token)
     return listOfTokens, max_length
 
-
-def convert_token_to_embedding(listOfTokens, listOfScores): 
+def get_humor_score(word, humorDict):
+  word=word.lower()
+  if word in humorDict.keys():
+    return float(humorDict[word])
+  else:
+    return 0
+    
+def convert_token_to_embedding(listOfTokens, listOfScores, humor): 
     #This model is trained off of the vocabulary in the training set
     
-    embedding_length = 100
-    model = Word2Vec(listOfTokens,size=embedding_length)
+    embedding_length = 101
+    model = Word2Vec(listOfTokens,size=embedding_length-1)
     
     vocab = list(model.wv.vocab)
     
@@ -97,10 +114,16 @@ def convert_token_to_embedding(listOfTokens, listOfScores):
     #loop through each word in each sentence to get the embeddings
         for word in sentence:
             if word in vocab:
-                listOfEmbeddings.append(model[word])
-                embedding_length = len(model[word])
+              h=get_humor_score(word,humor)
+              vec=model[word]
+              vec=numpy.append(vec,h)
+              listOfEmbeddings.append(vec)
+              embedding_length = len(vec)
             else:
-                listOfEmbeddings.append([0]*embedding_length)
+              h=get_humor_score(word,humor)
+              vec=[0]*(embedding_length-1)
+              vec.append(h)
+              listOfEmbeddings.append(vec)  
         listOfEmbeddings = torch.tensor(listOfEmbeddings, dtype=torch.float)
         try:
             input_score.append(float(score))
@@ -111,11 +134,11 @@ def convert_token_to_embedding(listOfTokens, listOfScores):
         
     return input_list, input_score, vocab, model, max_length
 
-def get_input(filePath): 
+def get_input(filePath, humor): 
     print("Getting tokens...")
-    listOfTokens, listOfScores = get_tokens(filePath)
+    listOfTokens, listOfScores= get_tokens(filePath)
     print("Converting tokens to embeddings...")
-    input_list, input_scores, vocab, model, max_length = convert_token_to_embedding(listOfTokens, listOfScores)
+    input_list, input_scores, vocab, model, max_length = convert_token_to_embedding(listOfTokens, listOfScores, humor)
     input_list = input_list[2:]
     print("Finished converted tokens to embeddings...")
     
@@ -130,6 +153,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class LSTM(torch.nn.Module):
+  def __init__(self, vocabSize, numTags, hiddenSize):
+    super(LSTM, self).__init__()
+    
+    self.embed=torch.nn.Embedding(vocabSize, hiddenSize)
+    #self.lstm=torch.nn.LSTM(hiddenSize, hiddenSize, batch_first=True, bidirectional=False)
+    self.lstm=torch.nn.LSTM(numTags, hiddenSize, batch_first=True, bidirectional=False)
+    self.lin=torch.nn.Linear(hiddenSize, numTags)
+    
+    self.norm=torch.nn.LayerNorm(hiddenSize)
+    
+  def forward(self, batchFeats):
+    #embeds = self.embed(batchFeats)
+    lstm_out, _ = self.lstm(batchFeats)
+    tag_space = self.lin(lstm_out)
+    tag_scores = torch.sigmoid(tag_space)
+    return lstm_out
+
 class CNN(nn.Module):
     def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
                  dropout):
@@ -138,15 +179,15 @@ class CNN(nn.Module):
         
         #self.embedding = nn.Embedding(vocab_size, embedding_dim)
         
-        self.conv_0 = nn.Conv2d(in_channels = 1, 
+        self.conv_0 = nn.Conv1d(in_channels = 1, 
                                 out_channels = n_filters, 
                                 kernel_size = (filter_sizes[0], embedding_dim))
         
-        self.conv_1 = nn.Conv2d(in_channels = 1, 
+        self.conv_1 = nn.Conv1d(in_channels = 1, 
                                 out_channels = n_filters, 
                                 kernel_size = (filter_sizes[1], embedding_dim))
         
-        self.conv_2 = nn.Conv2d(in_channels = 1, 
+        self.conv_2 = nn.Conv1d(in_channels = 1, 
                                 out_channels = n_filters, 
                                 kernel_size = (filter_sizes[2], embedding_dim))
         
@@ -183,7 +224,7 @@ class CNN(nn.Module):
         #cat = [batch size, n_filters * len(filter_sizes)]
         
         fc = self.fc(cat)
-        
+        #fc=(fc*10).round()/10
         return fc
     
 def count_parameters(model):
@@ -216,13 +257,13 @@ def train(model, inputs, outputs, optimizer, criterion):
     
     for idx, input_batch in enumerate(inputs): 
         if idx == 0: 
-            input_size = len(input_batch)
-            
+            input_size = len(input_batch)  
         output_batch = outputs[idx]
         
         predictions = model(input_batch)
-        
+      
         if len(input_batch) == input_size: 
+            #print(len(input_batch))
             predictions = torch.reshape(predictions, [len(input_batch),1])
             output_batch = torch.reshape(output_batch, [len(input_batch),1])
             
@@ -230,7 +271,9 @@ def train(model, inputs, outputs, optimizer, criterion):
             if binary_accuracy(predictions, output_batch):
                 correct += 1
             total +=1
-                
+            
+            #print(predictions)
+            #print(output_batch)
             loss = criterion(predictions, output_batch)
             
             loss.backward()
@@ -278,8 +321,8 @@ def eval_to_max_length(headline, max_length):
             headline.append("<PAD>")
     return headline
 
-def tests_to_embeddings(max_length, tests, vector_model,vocab): 
-    embedding_length = 100
+def tests_to_embeddings(max_length, tests, vector_model,vocab, humor): 
+    embedding_length = 101
     ret = []
     for test in tests: 
         h1 = test[0].split(" ")
@@ -291,17 +334,29 @@ def tests_to_embeddings(max_length, tests, vector_model,vocab):
         tmp_h1 = []
         for word in h1:
             if word in vocab:
-                tmp_h1.append(vector_model[word])
+              h=get_humor_score(word,humor)
+              vec=vector_model[word]
+              vec=numpy.append(vec,h)
+              tmp_h1.append(vec)
             else:
-                tmp_h1.append([0]*embedding_length)
+              h=get_humor_score(word,humor)
+              vec=[0]*(embedding_length-1)
+              vec.append(h)
+              tmp_h1.append(vec)
         h1 = tmp_h1
         
         tmp_h2 = []
         for word in h2:
             if word in vocab: 
-                tmp_h2.append(vector_model[word])
+              h=get_humor_score(word,humor)
+              vec=vector_model[word]
+              vec=numpy.append(vec,h)
+              tmp_h2.append(vec)
             else: 
-                tmp_h2.append([0]*embedding_length) 
+              h=get_humor_score(word,humor)
+              vec=[0]*(embedding_length-1)
+              vec.append(h)
+              tmp_h2.append(vec) 
         h2 = tmp_h2 
         
         h1 = torch.tensor(h1, dtype=torch.float)
@@ -316,10 +371,10 @@ def tests_to_embeddings(max_length, tests, vector_model,vocab):
         ret.append((h1, h2, label))
     return ret
 
-def evaluate(filePath, model, vector_model, max_length, vocab) :
-    embedding_length = 100
+def evaluate(filePath, model, vector_model, max_length, vocab, humor) :
+    embedding_length = 101
     tests = get_test_file(filePath)
-    embedded_tests = tests_to_embeddings(max_length, tests, vector_model, vocab)[2:]
+    embedded_tests = tests_to_embeddings(max_length, tests, vector_model, vocab, humor)[2:]
     
     chunked_embedded_tests = chunks(embedded_tests, 10)
     chunked_tests = chunks(tests, 10) 
@@ -370,11 +425,71 @@ def evaluate(filePath, model, vector_model, max_length, vocab) :
             for idx, predicted in enumerate(actual_labels): 
                 actual = chunk_labels[idx]
                 if predicted == actual: total_correct += 1
-                total += 1
+                if not actual==0:
+                  total += 1
                 
     print (total_correct/float(total))
                 
+def evaluate2(filePath, model, vector_model, max_length, vocab, humor) :
+    import csv
+    output_csv = open('output.csv', 'w')
+    output_writer = csv.writer(output_csv)
+    
+    embedding_length = 101
+    tests = get_test_file(filePath)
+    embedded_tests = tests_to_embeddings(max_length, tests, vector_model, vocab, humor)[2:]
+    
+    chunked_embedded_tests = chunks(embedded_tests, 10)
+    chunked_tests = chunks(tests, 10) 
+    
+    
+    tmp_tests = []
+    for c in chunked_tests:
+        tmp_tests.append(c)
         
+    total_correct = 0
+    total = 0 
+    for chunk_idx, chunk in enumerate(chunked_embedded_tests):
+        c1_list = []
+        c2_list = []
+        chunk_labels = []
+        
+        if len(chunk) == 10: 
+            for test in chunk: 
+                c1_list.append(test[0])
+                c2_list.append(test[1])
+                chunk_labels.append(test[2])
+            
+            c1_scores = model(torch.stack(c1_list))
+            c2_scores = model(torch.stack(c2_list))
+            
+            
+            ### Test if headlines are correctly paired
+            ## This prints the first word embedding of the first test in both clusters
+            ## It prints the cluster ID before the first word embedding of the first test of that cluster
+            
+            actual_labels = []
+            for idx, c1_score in enumerate(c1_scores):
+                label = 0
+                
+                
+                if c1_score > c2_scores[idx]: 
+                    label = 1
+                if c1_score < c2_scores[idx]:
+                    label = 2 
+                actual_labels.append(label)
+            
+            for idx, i in enumerate(chunk_labels): 
+                chunk_labels[idx] = int(i[0])
+                
+                
+            for idx, predicted in enumerate(actual_labels): 
+                output_writer.writerow([tmp_tests[chunk_idx][idx], predicted])
+                actual = chunk_labels[idx]
+                if predicted == actual: total_correct += 1
+                total += 1
+                
+    print (total_correct/float(total))       
     
 
 def get_test_file(filePath): 
@@ -434,17 +549,20 @@ def chunks(lst, n):
         yield lst[i:i + n]
         
 def main(): 
-    tokens, inputs, input_scores, vocab, vector_model, max_length = get_input("tmp2.csv")
+    humor=get_humor()
+    tokens, inputs, input_scores, vocab, vector_model, max_length = get_input("tmp2.csv", humor)
     
+    #for i, score in enumerate(input_scores):
+      #input_scores[i]=round(input_scores[i],1)
     input_scores = torch.tensor(input_scores, dtype=torch.float)
     
     print("Doing setup...")
     INPUT_DIM = max_sentence_length(tokens)
-    EMBEDDING_DIM = 100
+    EMBEDDING_DIM = 101
     N_FILTERS = 100
     FILTER_SIZES = [2,3,4]
     OUTPUT_DIM = 1
-    DROPOUT = 0.5
+    DROPOUT = 0.2
     #PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
     
     ### Condense inputs into Tensor [ #Examples, Sentence_Length, EmbeddingSize]
@@ -457,25 +575,25 @@ def main():
     
     print("Make model..")
     model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT)
+    #model = LSTM(INPUT_DIM, EMBEDDING_DIM, OUTPUT_DIM)
     
     import torch.optim as optim
-    optimizer = optim.Adam(model.parameters())
-    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=.001)
+    criterion = nn.MSELoss()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
     criterion = criterion.to(device)
     
     
-    N_EPOCHS = 1
+    N_EPOCHS = 5
 
     best_valid_loss = float('inf')
     
     for epoch in range(N_EPOCHS):
-        print("Epoch: " + str(epoch))
         train_loss = train(model, inputs, input_scores, optimizer, criterion)
         print(train_loss)
     
-    tmp = evaluate("test.csv", model, vector_model, max_length, vocab)
+    tmp = evaluate2("test.csv", model, vector_model, max_length, vocab, humor)
     
     return model, inputs, input_scores, tmp
 
